916b005396cb8ef504b1bd7709688f668739632a
Dan Bearl <dan.bearl@gmail.com>
2015-09-13 21:23:13 -0400

    Generating sessions controller and views

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  
+  def create
+    user = User.find_by(email: params[:email].downcase)
+    if user && user.authenticate(params[:password])
+      sign_in(user)
+      redirect_back_or_to(root_path, notice: "Welcome back!")
+    else
+      flash.now.alert = "Your email and password do not match"
+      render :new
+    end
+  end
+
+  def destroy
+    sign_out_user
+    redirect_to :root, notice: "Logged out successfully."
+  end
+
+end
